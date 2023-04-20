a6a9e4ca7c598d4a9856103c65029f4ea82e66a9
tdhood <taylordannhood@gmail.com>
2023-04-20 10:04:17 -0700

    testing added for louisiana

---
+++ tests/test_louisiana.py
@@ -0,0 +1,21 @@
+import unittest
+import requests
+
+from lottery_data_scraper import louisiana
+from lottery_data_scraper import schemas
+
+class TestLouisiana(unittest.TestCase):
+    def test_parse_game_html(self):
+        # URL chosen arbitrarily
+        url = 'https://louisianalottery.com/scratch-offs/1450/blazing-suits'
+        html = louisiana.fetch_html(url)
+        game = louisiana.parse_game(url, html)
+        self.assertEqual(game['name'], 'Blazing Suits')
+        self.assertEqual(game["price"], 10.0)
+        self.assertEqual(
+            game["url"],
+           'https://louisianalottery.com/scratch-offs/1450/blazing-suits' ,
+        )
+        self.assertEqual(game["game_id"], "1450")
+        self.assertEqual(game["prizes"][0]["prize"], "$200,000")
+        self.assertEqual(game["prizes"][0]["value"], 200000.0)
