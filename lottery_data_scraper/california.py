426906cadbf50f8d969804589797b013e0f84850
Eric Ihli <eihli@owoga.com>
2023-04-23 15:56:47 -0700

    Format with black and remove unused imports

--- lottery_data_scraper/california.py
+++ lottery_data_scraper/california.py
@@ -2,7 +2,6 @@ import locale
 import logging
 import json
 import operator
-import requests
 import html2text
 
 from lottery_data_scraper.schemas import GameSchema
@@ -12,13 +11,14 @@ from lottery_data_scraper.util import fetch_html
 # because California only gives prize values and our schema
 # expects a string representation of the prize.
 # https://docs.python.org/3/library/locale.html
-locale.setlocale(locale.LC_ALL, 'en_US.utf8')
+locale.setlocale(locale.LC_ALL, "en_US.utf8")
 
 logger = logging.getLogger(__name__)
 h = html2text.HTML2Text()
 
 BASE_URL = "https://www.calottery.com"
 SCRATCHER_URL = "https://www.calottery.com/api/games/scratchers"
+
 
 def num_tx_initial(game):
     grand_prize = game["topPrizeTier"]
@@ -35,7 +35,9 @@ def fetch_games():
                 "available": prize_["numberOfPrizesPending"],
                 "claimed": prize_["numberOfPrizesCashed"],
                 "value": prize_["value"],
-                "prize": locale.currency(prize_["value"], grouping=True)[:-3] # -3 to drop the cents
+                "prize": locale.currency(prize_["value"], grouping=True)[
+                    :-3
+                ],  # -3 to drop the cents
             }
             prizes.append(prize)
         grand_prize = sorted(game_["prizeTiers"], key=operator.itemgetter("value"))[-1]
