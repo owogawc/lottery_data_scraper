e39445282f621d7e4304b20aba77b5eaf4eaea85
tdhood <taylordannhood@gmail.com>
2023-05-01 16:28:13 -0700

    updated new mexico test

--- tests/test_new_mexico.py
+++ tests/test_new_mexico.py
@@ -11,10 +11,10 @@ class TestNewMexico(unittest.TestCase):
         html = new_mexico.fetch_html(url)
         game = new_mexico.get_games(url)[0]
         game = new_mexico.process_game(game)
-        self.assertEqual(game["name"], "$5,000 ORO")
-        self.assertEqual(game["price"], 2)
-        self.assertEqual(game["game_id"], "545")
-        self.assertEqual(game["prizes"][0]["prize"], "$5,000")
+        self.assertEqual(game["name"], "Match 3 Tripler")
+        self.assertEqual(game["price"], 1)
+        self.assertEqual(game["game_id"], "521")
+        self.assertEqual(game["prizes"][0]["prize"], "$900")
         # Perhaps unfortunately in dollars. Cents would be better, eh?
-        self.assertEqual(game["prizes"][0]["value"], 5000)
-        self.assertEqual(game["num_tx_initial"], 498000)
+        self.assertEqual(game["prizes"][0]["value"], 900)
+        self.assertEqual(game["num_tx_initial"], 670800)
